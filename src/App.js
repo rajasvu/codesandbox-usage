2fdf672adc8332f1ef3fceaf10b5ea6cb3aaa269
Gattamaneni Kumar Raja <rajasvu@gmail.com>
2021-06-12 22:09:48 +0530

    second commit A commit done after PR is raised

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello React from Codesandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Trying for branching</h2>
     </div>
   );
 }
